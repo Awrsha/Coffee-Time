7ec52464003c48fb0ecb4faa608d54bbadee074b
Amir M. Parvizi <official.parvizi@gmail.com>
2024-11-11 19:11:47 +0330

    Update dcm.ino

--- School Crossing Sign (for Autonomous Vehicles)/dcm/dcm.ino
+++ School Crossing Sign (for Autonomous Vehicles)/dcm/dcm.ino
@@ -1,23 +1,31 @@
-char incomingData;
+char incomingData;  // Variable to store incoming serial data
+
 void setup() {
-pinMode(2,OUTPUT);
-pinMode(5,OUTPUT);
-pinMode(6,OUTPUT);
-digitalWrite(2,HIGH);
-Serial.begin(9600);
+  // Set up digital pins as output pins
+  pinMode(2, OUTPUT);   // Configure pin 2 as output for potential future use
+  pinMode(5, OUTPUT);   // Configure pin 5 as output (controls PWM for LED brightness)
+  pinMode(6, OUTPUT);   // Configure pin 6 as output for potential future use
+  
+  // Initialize pin 2 to HIGH to turn on any connected component initially
+  digitalWrite(2, HIGH);
+
+  // Begin serial communication at a baud rate of 9600 bps
+  Serial.begin(9600);
 }
 
 void loop() {
-  if(Serial.available()>0)
-  {
-    incomingData=Serial.read();
-    if(incomingData=='a')
-    {
-      analogWrite(5,75);
-      }
-     else{
-      analogWrite(5,255);
-      }
-    }
+  // Check if there is incoming serial data
+  if (Serial.available() > 0) {
+    // Read the incoming byte of data
+    incomingData = Serial.read();
 
+    // If the received data is 'a', set pin 5 to a lower PWM value (dim light)
+    if (incomingData == 'a') {
+      analogWrite(5, 75);  // Set pin 5 to 75, which dims the LED or connected device
+    }
+    // For any other data, set pin 5 to maximum PWM value (full brightness)
+    else {
+      analogWrite(5, 255); // Set pin 5 to 255, providing full brightness
+    }
+  }
 }
